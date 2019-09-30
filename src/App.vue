c0d038c47c45dcdfe26077dc155ac4a74a627c48
zqwei <zqwei@iflytek.com>
2019-09-30 18:41:08 +0800

    增加devServer配置，增加网易云音乐固定歌单列表爬取展示

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,18 @@
         <div class="img-container"><img v-bind:src="img" /></div>
       </el-carousel-item>
     </el-carousel>
+    <el-table :data="tableData" style="width: 100%" row-key="id">
+      <el-table-column
+        property="title"
+        label="歌曲"
+        min-width="30%"
+      ></el-table-column>
+      <el-table-column property="url" label="播放" min-width="70%">
+        <template slot-scope="scope">
+          <audio controls :src="scope.row.url" />
+        </template>
+      </el-table-column>
+    </el-table>
   </div>
 </template>
 
@@ -19,8 +31,14 @@ export default {
         require("./assets/imgs/2.jpg"),
         require("./assets/imgs/3.jpg"),
         require("./assets/imgs/4.jpg")
-      ]
+      ],
+      tableData: []
     };
+  },
+  async mounted() {
+    const res = await this.$axios.get("/songs?playlist=2994239216");
+    console.log(res)
+    this.tableData = res.data;
   }
 };
 </script>
