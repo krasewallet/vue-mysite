3feebf118aa37a37f54e56c14d511273f19a80a8
zqwei <zqwei@iflytek.com>
2019-09-30 19:35:53 +0800

    调整样式，滚动条仅在歌曲列表有效

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,38 @@
 <template>
   <div id="app">
     <div class="header"></div>
-    <el-carousel :interval="5000" arrow="always">
-      <el-carousel-item v-for="(img, index) in imgList" :key="index">
-        <div class="img-container"><img v-bind:src="img" /></div>
-      </el-carousel-item>
-    </el-carousel>
-    <el-table :data="tableData" style="width: 100%" row-key="id">
-      <el-table-column
-        property="title"
-        label="歌曲"
-        min-width="30%"
-        align="center"
-      ></el-table-column>
-      <el-table-column
-        property="url"
-        label="播放"
-        min-width="70%"
-        align="center"
+    <div class="carousel-container">
+      <el-carousel :interval="5000" arrow="always">
+        <el-carousel-item v-for="(img, index) in imgList" :key="index">
+          <div class="img-container"><img v-bind:src="img" /></div>
+        </el-carousel-item>
+      </el-carousel>
+    </div>
+    <div class="table-container">
+      <el-table
+        :data="tableData"
+        style="width: 100%"
+        row-key="id"
+        height="100%"
       >
-        <template slot-scope="scope">
-          <audio controls :src="scope.row.url" />
-        </template>
-      </el-table-column>
-    </el-table>
+        <el-table-column
+          property="title"
+          label="歌曲"
+          min-width="30%"
+          align="center"
+        ></el-table-column>
+        <el-table-column
+          property="url"
+          label="播放"
+          min-width="70%"
+          align="center"
+        >
+          <template slot-scope="scope">
+            <audio controls :src="scope.row.url" />
+          </template>
+        </el-table-column>
+      </el-table>
+    </div>
   </div>
 </template>
 
@@ -43,7 +52,7 @@ export default {
   },
   async mounted() {
     const res = await this.$axios.get("/songs?playlist=2994239216");
-    console.log(res)
+    console.log(res);
     this.tableData = res.data;
   }
 };
@@ -56,6 +65,9 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   background-color: lightblue;
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
 }
 .header {
   height: 60px;
@@ -67,5 +79,11 @@ export default {
 }
 .img-container img {
   height: 100%;
+}
+.carousel-container {
+  height: 300px;
+}
+.table-container {
+  height: calc(100% - 360px);
 }
 </style>
