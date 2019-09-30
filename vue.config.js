8f8f9f48cb0593643d3635e415c316e987b05586
zqwei <zqwei@iflytek.com>
2019-09-30 19:39:18 +0800

    lint auto fix

--- vue.config.js
+++ vue.config.js
@@ -7,7 +7,7 @@ module.exports = {
           //在ajax请问的url参数前 加上'/API'就行了，此时的'/API'就是 'https://www.easy-mock.com'的变量。
           target: "http://101.132.68.147/",
           ws: true,
-          changeOrigin: true,
+          changeOrigin: true
           // pathRewrite: {
           //   "^/API": ""
           // }
